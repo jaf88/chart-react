e8aafb92708b0e49e207cca96c1f2441c669ccd0
jaf88 <jaf.alves8@gmail.com>
2021-02-18 11:36:06 -0300

    chore: configurations initials

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,15 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+        
+    return (
+        <div>
+            ChatFeed
+        </div>
+    );
+}
+
+export default ChatFeed;
